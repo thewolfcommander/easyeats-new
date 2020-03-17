6a5b50b9693783534e4f9b11a237c2035ff9317e
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-03-17 14:45:22 +0530

    Categories detail updated

--- third/views.py
+++ third/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+
 from accounts.models import User, Vendor, DeliveryBoy
 from accounts.forms import VendorForm, DeliveryBoyForm
 
