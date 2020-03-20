cba470719bbf8b4bc2ac1521adc48260f30adbf5
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-03-20 20:01:02 +0530

    Shipping algorithm updated

--- cart/models.py
+++ cart/models.py
@@ -49,18 +49,22 @@ def pre_save_cart_reciever(sender, instance, action, *args, **kwargs):
         for food in foods:
             sub_total += float(food.discount_price)
         instance.sub_total = sub_total
-        if instance.user.order_count != 0:
-            if instance.sub_total > 0:
-                if instance.sub_total < 80.00:
-                    instance.shipping = 25.00
-                elif instance.sub_total > 80.00 and instance.sub_total < 280.00:
-                    instance.shipping = 30.00
-                else:
-                    instance.shipping = 25.00
+
+        # calculating the shipping
+        if instance.sub_total > 0:
+            if instance.sub_total < 80.00:
+                instance.shipping = 25.00
+            elif instance.sub_total > 80.00 and instance.sub_total < 280.00:
+                instance.shipping = 30.00
             else:
-                instance.shipping = 0.00
+                instance.shipping = 25.00
         else:
             instance.shipping = 0.00
+
+        # Checking for the user is ordering the food for first time or not
+        if instance.user.is_authenticated:
+            if instance.user.order_count == 0:
+                instance.shipping = 0.00
         instance.total = instance.shipping + instance.sub_total
         instance.save()
 
