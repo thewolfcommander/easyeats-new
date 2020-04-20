cf0d2d123667a5cc2f2f05edfa7d359c916ff5a3
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-20 22:05:30 +0530

    Categories, ub-categories and other details added

--- easyeats/urls.py
+++ easyeats/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # Redirects
+    path('', RedirectView.as_view(url='groceries/')),
+
+    # Normal Views
     path('', include('core.urls', namespace='core')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
