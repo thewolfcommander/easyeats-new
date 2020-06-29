a997b9acc94f9ccb1286c83e9736a6dda86b361d
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-06-29 12:56:31 +0530

    Configuring Push Notifications

--- static/js/main.js
+++ static/js/main.js
@@ -93,3 +93,17 @@ $(function() {
 });
 
 
+
+
+const check = () => {
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('No Service Worker support!')
+    }
+    if (!('PushManager' in window)) {
+      throw new Error('No Push API Support!')
+    }
+  }
+  const main = () => {
+    check()
+  }
+  main()
